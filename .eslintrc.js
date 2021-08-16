042604953d696c1c2b267abeab807a2e03b5b676
Prokopenkon Nikolay <prokopenko.nn@reliab.tech>
2021-08-16 21:32:29 +0300

    prettier and eslint config edited

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+	parserOptions: {
+		parser: "babel-eslint"
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		semi: ["error", "never"],
+		quotes: ["error", "double"],
+		"no-multi-spaces": "error",
+		"no-multiple-empty-lines": ["error", { max: 1 }],
+		"function-paren-newline": ["error", { minItems: 3 }],
+		"eol-last": ["error", "always"],
+		"no-duplicate-imports": "error"
+	}
+}
