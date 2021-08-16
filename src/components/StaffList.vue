535a18857d6b179f4f36eaad9a23559469ab2858
Prokopenkon Nikolay <prokopenko.nn@reliab.tech>
2021-08-16 22:10:28 +0300

    staff list done

---
+++ src/components/StaffList.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-data-table :headers="headers" :items="staff" hide-default-footer @click:row="staffSelected">
+	</v-data-table>
+</template>
+
+<script>
+export default {
+	name: "StaffList", props: {
+		staff: Array,
+		staffSelected: Function
+	}, data() {
+		return {
+			headers: [{
+				text: "ФИО сотрудника", align: "center", sortable: false, value: "fio"
+			}]
+		}
+	}
+}
+</script>
--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -1,10 +1,51 @@
 <template>
-	<div>test</div>
+	<v-row>
+		<v-col>
+			<div class="btnWrapper">
+				<v-btn @click="createStaff">Добавить</v-btn>
+			</div>
+			<StaffList :staff="staffWithInitials" :staff-selected="staffSelected" />
+		</v-col>
+		<v-col>
+
+		</v-col>
+	</v-row>
 </template>
 
 <script>
+import StaffList from "../components/StaffList"
 
 export default {
 	name: "Staff",
+
+	components: {
+		StaffList
+	}, data() {
+		return {
+			staff: [], selectedStaff: null
+		}
+	}, computed: {
+		staffWithInitials: function() {
+			return this.staff ? this.staff.map(s => ({
+				id: s.id,
+				fio: s.fio.split(" ").map((item, index) => index === 0 ? item : `${item[0].toUpperCase()}.`).join(" ")
+			})) : []
+		}
+	}, methods: {
+		staffSelected(staff) {
+			this.selectedStaff = staff
+		}, createStaff() {
+			this.selectedStaff = {
+				fio: "", pass_ser: "", pass_no: "", pass_dt: ""
+			}
+		}
+	}
 }
 </script>
+
+<style scoped>
+.btnWrapper {
+	display: flex;
+	justify-content: center;
+}
+</style>
